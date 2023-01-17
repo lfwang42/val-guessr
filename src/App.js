2e966364ce12061a590be7390274866568e81aa3
Lanney Wang <lfwang42@gmail.com>
2023-01-16 20:40:15 -0500

    added homepage, logo + credits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import logo from './valorant logo.png';
 import './App.css';
+import Footer from './footer.js'
 
 function App() {
   return (
+    /* "_id": "new9",
+    "name": "pic49.jpg",
+    "lat": "1056",
+    "lng": "600",
+    "location": "Teyvat"
+}*/
+//images stored locally (?), but have id, names, and store their lat/longitude locally
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="main-logo" alt="logo" />
+        <h2> ValGuessr</h2>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/footer.js
@@ -0,0 +1,17 @@
+import './footer.css';
+
+function Footer() {
+    return (
+      /* "_id": "new9",
+      "name": "pic49.jpg",
+      "lat": "1056",
+      "lng": "600",
+      "location": "Teyvat"
+  }*/
+  //images stored locally (?), but have id, names, and store their lat/longitude locally
+    <div className="footer">Created by <div class = "author"><a class = "footerlink" href="https://www.google.com">Lanney Wang.</a></div>
+    </div>
+    );
+  }
+
+export default Footer;
