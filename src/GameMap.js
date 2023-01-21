55fb77ea14aee9d3d0c1f7af328007376b770212
Lanney Wang <lfwang42@gmail.com>
2023-01-20 22:23:36 -0500

    added first minimap for pearl

---
+++ src/GameMap.js
@@ -0,0 +1,62 @@
+import './GameMap.css';
+import React, { useState } from 'react';
+import { MapContainer, Marker, useMapEvents, ImageOverlay, TileLayer, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {CRS, Icon, marker} from 'leaflet'
+
+function GameMap(props) {
+    const bounds = [
+        [0, 0],
+        [1024, 1024]
+      ];
+    const maxBounds = [[0,0],[1024,1024]];
+    
+    function LocationMarker() {
+      const [position, setPosition] = useState(null)
+      const map = useMapEvents({
+        click(e) {
+          setPosition([e.latlng.lat, e.latlng.lng]);     
+        },
+      });
+    
+      return position === null ? null : (
+        <Marker icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}  position={position}>
+            <Popup>
+                {position[0]}, {position[1]}
+            </Popup>
+        </Marker>
+      )
+    }
+
+    return (
+      /* "_id": "new9",
+      "name": "pic49.jpg",
+      "lat": "1056",
+      "lng": "600",
+      "location": "Teyvat"
+  }*/
+  //           <LocationMarker position={[0, 0]} />    bounds = {[[0,0], [20, 20]]}        url="https://static.wikia.nocookie.net/valorant/images/6/63/Pearl_minimap.png"
+  //images stored locally (?), but have id, names, and store their lat/longitude locally
+  //<img class = "MainImage" src="https://f004.backblazeb2.com/file/cldimglt/pic15.jpg" />
+        <MapContainer center = {[500,500]} 
+        zoom={-1} 
+        maxZoom={2}
+        minZoom={-1}
+        scrollWheelZoom={true} 
+        maxBounds = {maxBounds}
+        crs={CRS.Simple}
+        whenCreated={ mapInstance => { this.mapRef.current = mapInstance } }>
+          <ImageOverlay
+            attribution='&copy; <a href="https://valorant.fandom.com">Valorant Wiki</a>'
+            url = "https://static.wikia.nocookie.net/valorant/images/6/63/Pearl_minimap.png"
+            bounds= {bounds}
+            zIndex = {1}
+          />
+          <LocationMarker />
+        </MapContainer>       
+ 
+    );
+  }
+
+export default GameMap;
--- src/MainGame.js
+++ src/MainGame.js
@@ -1,14 +1,29 @@
 import './MainGame.css';
 import React, { useState } from 'react';
-import { MapContainer, TileLayer, Popup, Marker, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup, Marker, useMapEvents, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import markerIconPng from "leaflet/dist/images/marker-icon.png"
-import {Icon} from 'leaflet'
+import {Icon, marker} from 'leaflet'
+import GameMap from './GameMap';
 
 function MainGame(props) {
     const [gameEnded, startGame] = React.useState(false);
     const [round, setRound] = React.useState(1);
     const [pics, setPics] = useState([{"name": './media/1.jpg'}, {"name":'./media/2.jpg'}, {"name": './media/3.jpg'}])
+
+    
+    function LocationMarker() {
+      const [position, setPosition] = useState(null)
+      const map = useMapEvents({
+        click(e) {
+          setPosition([e.latlng.lat, e.latlng.lng]);     
+        },
+      });
+    
+      return position === null ? null : (
+        <Marker icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}  position={position} />
+      )
+    }
 
     return (
       /* "_id": "new9",
@@ -22,14 +37,9 @@ function MainGame(props) {
     <div className="MainGame">
       <div className="MainDisplay">
         
-        <MapContainer style={{ height: "450px", width: "100%" }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
-          </Marker>
-        </MapContainer>
+        <div className="MapBox">
+          <GameMap />
+        </div>
       </div>        
     </div>
  
