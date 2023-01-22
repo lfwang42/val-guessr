97d9d3e7e890f0c84e59e274b76d92e99b6b5ca7
Lanney Wang <lfwang42@gmail.com>
2023-01-21 21:45:53 -0500

    added some map images

--- src/MainGame.js
+++ src/MainGame.js
@@ -9,8 +9,8 @@ import GameMap from './GameMap';
 function MainGame(props) {
     const [gameEnded, startGame] = React.useState(false);
     const [round, setRound] = React.useState(1);
-    const [pics, setPics] = useState([{"name": './media/1.jpg'}, {"name":'./media/2.jpg'}, {"name": './media/3.jpg'}])
-
+    const [pics, setPics] = useState([{"name": 'k4m1DPP/2023-01-21-3.png', "coords": [500, 500]}]);
+    const currentPic = "https://i.ibb.co/"+pics[0].name;
     
     function LocationMarker() {
       const [position, setPosition] = useState(null)
@@ -36,7 +36,9 @@ function MainGame(props) {
   //<img class = "MainImage" src="https://f004.backblazeb2.com/file/cldimglt/pic15.jpg" />
     <div className="MainGame">
       <div className="MainDisplay">
-        
+        <div className = "Viewer" >
+          <img className = "ViewImage" src = "https://i.ibb.co/k4m1DPP/2023-01-21-3.png" />
+        </div>
         <div className="MapBox">
           <GameMap />
         </div>
